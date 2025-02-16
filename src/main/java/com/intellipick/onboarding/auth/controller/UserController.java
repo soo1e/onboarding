7ae7f3c6ae2b13d1dc6eabde27fa285652b80cf7
soo1e <gameswuon@gmail.com>
2025-02-16 12:35:12 +0900

    :sparkles: 컨트롤러에 적용

--- src/main/java/com/intellipick/onboarding/auth/controller/UserController.java
+++ src/main/java/com/intellipick/onboarding/auth/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.intellipick.onboarding.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +15,14 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "회원가입 API", description = "사용자 회원가입 관련 API")
 public class UserController {
 
 	private final UserService userService;
 
 	@PostMapping("/api/signup")
+	@Operation(summary = "회원가입 API", description = "사용자의 정보를 입력받아 회원가입을 처리합니다.")
 	public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest request) {
 		return ResponseEntity.ok(userService.signup(request));
 	}
-
 }
