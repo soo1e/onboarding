59dd9e7b8f9cee7239413bf4c32a34e5663c59e8
soo1e <gameswuon@gmail.com>
2025-03-15 19:54:15 +0900

    :sparkles: 수정 메서드 추가

--- src/main/java/com/intellipick/onboarding/auth/entity/User.java
+++ src/main/java/com/intellipick/onboarding/auth/entity/User.java
@@ -27,4 +27,14 @@ public class User {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Role role;
+
+	public User updateRole(Role newRole) {
+		return User.builder()
+			.id(this.id)
+			.username(this.username)
+			.password(this.password)
+			.nickname(this.nickname)
+			.role(newRole)
+			.build();
+	}
 }
