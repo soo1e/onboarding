f42544ee78c483343ce5b98c0a482f168b4d1e79
soo1e <gameswuon@gmail.com>
2025-03-15 19:57:03 +0900

    :sparkles: Auth 수정

--- src/main/java/com/intellipick/onboarding/auth/controller/AuthController.java
+++ src/main/java/com/intellipick/onboarding/auth/controller/AuthController.java
@@ -23,17 +23,9 @@ public class AuthController {
 
 	private final AuthService authService;
 
-	@PostMapping("/api/login")
+	@PostMapping("/login")
 	@Operation(summary = "로그인 API", description = "사용자의 아이디와 비밀번호를 입력받아 JWT 토큰을 반환합니다.")
 	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
 		return ResponseEntity.ok(authService.login(request));
-	}
-
-	@GetMapping("/admin")
-	@Operation(summary = "관리자 접근 API", description = "관리자만 접근 가능한 페이지입니다. 관리자 ID : admin, Password : qwer1234")
-	public ResponseEntity<String> adminAccess() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println("[AdminController] 현재 인증 정보: " + auth);
-		return ResponseEntity.ok("관리자만 접근 가능합니다! 당신은 관리자군요");
 	}
 }
--- src/main/java/com/intellipick/onboarding/auth/service/AuthService.java
+++ src/main/java/com/intellipick/onboarding/auth/service/AuthService.java
@@ -1,10 +1,12 @@
 package com.intellipick.onboarding.auth.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.intellipick.onboarding.auth.dto.LoginRequest;
 import com.intellipick.onboarding.auth.dto.LoginResponse;
 import com.intellipick.onboarding.auth.entity.User;
+import com.intellipick.onboarding.auth.exception.InvalidCredentialsException;
 import com.intellipick.onboarding.auth.repository.UserRepository;
 import com.intellipick.onboarding.auth.security.JwtUtil;
 
@@ -16,10 +18,17 @@ public class AuthService {
 
 	private final UserRepository userRepository;
 	private final JwtUtil jwtUtil;
+	private final PasswordEncoder passwordEncoder;
+
 
 	public LoginResponse login(LoginRequest request) {
 		User user = userRepository.findByUsername(request.username())
-			.orElseThrow(() -> new IllegalArgumentException("유효하지 않는 사용자입니다."));
+			.orElseThrow(InvalidCredentialsException::new);
+
+		// 비밀번호 검증
+		if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+			throw new InvalidCredentialsException();
+		}
 
 		String token = jwtUtil.generateToken(user.getUsername(), user.getRole().name());
 
